ca619440d4e87bed6dd49a1330cad6717855aa64
tunght91 <hoangtung.utc@gmail.com>
2020-11-25 10:08:28 +0700

    crossSwapRouter

--- contracts/CrossSwapRouter.sol
+++ contracts/CrossSwapRouter.sol
@@ -7,173 +7,126 @@ import './uniswapv2/libraries/TransferHelper.sol';
 import './uniswapv2/interfaces/IUniswapV2Factory.sol';
 import './uniswapv2/interfaces/IUniswapV2Pair.sol';
 import './uniswapv2/interfaces/IWETH.sol';
+import './uniswapv2/UniswapV2Router02.sol';
 
-contract CrossSwapRouter {
+import './uniswapv2/libraries/UniswapV2Library.sol';
+
+contract CrossSwapRouter is 
+    UniswapV2Router02(
+        address(0x0388C1E0f210AbAe597B7DE712B9510C6C36C857), 
+        address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2)
+    ) 
+{
     using SafeMath for uint;
 
     address public constant factoryLua = address(0x0388C1E0f210AbAe597B7DE712B9510C6C36C857);
     address public constant factoryUni = address(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
     address public constant factorySushi = address(0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac);
-    address public constant WETH = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-
-    modifier ensure(uint deadline) {
-        require(deadline >= block.timestamp, 'UniswapV2Router: EXPIRED');
-        _;
-    }
-
-    receive() external payable {
-        assert(msg.sender == WETH); // only accept ETH via fallback from the WETH contract
-    }
 
     // **** SWAP ****
     // requires the initial amount to have already been sent to the first pair
-    function _swap(uint[] memory amounts, address[] memory path, address _to) internal virtual {
+    function _crossSwap(uint[] memory amounts, address[] memory path, address _to) internal virtual {
         for (uint i; i < path.length - 1; i++) {
             (address input, address output) = (path[i], path[i + 1]);
-            (address token0,) = _sortTokens(input, output);
+            (address token0,) = UniswapV2Library.sortTokens(input, output);
             uint amountOut = amounts[i + 1];
             (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOut) : (amountOut, uint(0));
-            address to = i < path.length - 2 ? _pairFor(output, path[i + 2]) : _to;
-            IUniswapV2Pair(_pairFor(input, output)).swap(
+            address to = i < path.length - 2 ? _crossPairFor(output, path[i + 2]) : _to;
+            IUniswapV2Pair(_crossPairFor(input, output)).swap(
                 amount0Out, amount1Out, to, new bytes(0)
             );
         }
     }
-    function swapExactTokensForTokens(
+    function crossSwapExactTokensForTokens(
         uint amountIn,
         uint amountOutMin,
         address[] calldata path,
         address to,
         uint deadline
     ) external ensure(deadline) returns (uint[] memory amounts) {
-        amounts = _getAmountsOut(amountIn, path);
+        amounts = getCrossAmountsOut(amountIn, path);
         require(amounts[amounts.length - 1] >= amountOutMin, 'UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT');
         TransferHelper.safeTransferFrom(
-            path[0], msg.sender, _pairFor(path[0], path[1]), amounts[0]
+            path[0], msg.sender, _crossPairFor(path[0], path[1]), amounts[0]
         );
-        _swap(amounts, path, to);
+        _crossSwap(amounts, path, to);
     }
-    function swapTokensForExactTokens(
+    function crossSwapTokensForExactTokens(
         uint amountOut,
         uint amountInMax,
         address[] calldata path,
         address to,
         uint deadline
     ) external ensure(deadline) returns (uint[] memory amounts) {
-        amounts = _getAmountsIn(amountOut, path);
+        amounts = getCrossAmountsIn(amountOut, path);
         require(amounts[0] <= amountInMax, 'UniswapV2Router: EXCESSIVE_INPUT_AMOUNT');
         TransferHelper.safeTransferFrom(
-            path[0], msg.sender, _pairFor(path[0], path[1]), amounts[0]
+            path[0], msg.sender, _crossPairFor(path[0], path[1]), amounts[0]
         );
-        _swap(amounts, path, to);
+        _crossSwap(amounts, path, to);
     }
 
-    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+    function crossSwapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
         external
         payable
         ensure(deadline)
         returns (uint[] memory amounts)
     {
         require(path[0] == WETH, 'UniswapV2Router: INVALID_PATH');
-        amounts = _getAmountsOut(msg.value, path);
+        amounts = getCrossAmountsOut(msg.value, path);
         require(amounts[amounts.length - 1] >= amountOutMin, 'UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT');
         IWETH(WETH).deposit{value: amounts[0]}();
-        assert(IWETH(WETH).transfer(_pairFor(path[0], path[1]), amounts[0]));
-        _swap(amounts, path, to);
+        assert(IWETH(WETH).transfer(_crossPairFor(path[0], path[1]), amounts[0]));
+        _crossSwap(amounts, path, to);
     }
 
-    function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+    function crossSwapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
         external
         ensure(deadline)
         returns (uint[] memory amounts)
     {
         require(path[path.length - 1] == WETH, 'UniswapV2Router: INVALID_PATH');
-        amounts = _getAmountsIn(amountOut, path);
-        require(amounts[0] <= amountInMax, 'UniswapV2Router: EXCESSIVE_INPUT_AMOUNT');
-        TransferHelper.safeTransferFrom(
-            path[0], msg.sender, _pairFor(path[0], path[1]), amounts[0]
-        );
-        _swap(amounts, path, address(this));
-        IWETH(WETH).withdraw(amounts[amounts.length - 1]);
-        TransferHelper.safeTransferETH(to, amounts[amounts.length - 1]);
-    }
-    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
-        external
-        ensure(deadline)
-        returns (uint[] memory amounts)
-    {
-        require(path[path.length - 1] == WETH, 'UniswapV2Router: INVALID_PATH');
-        amounts = _getAmountsOut(amountIn, path);
+        amounts = getCrossAmountsOut(amountIn, path);
         require(amounts[amounts.length - 1] >= amountOutMin, 'UniswapV2Router: INSUFFICIENT_OUTPUT_AMOUNT');
         TransferHelper.safeTransferFrom(
-            path[0], msg.sender, _pairFor(path[0], path[1]), amounts[0]
+            path[0], msg.sender, _crossPairFor(path[0], path[1]), amounts[0]
         );
-        _swap(amounts, path, address(this));
+        _crossSwap(amounts, path, address(this));
         IWETH(WETH).withdraw(amounts[amounts.length - 1]);
         TransferHelper.safeTransferETH(to, amounts[amounts.length - 1]);
     }
-    function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
-        external
-        payable
-        ensure(deadline)
-        returns (uint[] memory amounts)
-    {
-        require(path[0] == WETH, 'UniswapV2Router: INVALID_PATH');
-        amounts = _getAmountsIn(amountOut, path);
-        require(amounts[0] <= msg.value, 'UniswapV2Router: EXCESSIVE_INPUT_AMOUNT');
-        IWETH(WETH).deposit{value: amounts[0]}();
-        assert(IWETH(WETH).transfer(_pairFor(path[0], path[1]), amounts[0]));
-        _swap(amounts, path, to);
-        // refund dust eth, if any
-        if (msg.value > amounts[0]) TransferHelper.safeTransferETH(msg.sender, msg.value - amounts[0]);
-    }
+    // function crossSwapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+    //     external
+    //     ensure(deadline)
+    //     returns (uint[] memory amounts)
+    // {
+    //     require(path[path.length - 1] == WETH, 'UniswapV2Router: INVALID_PATH');
+    //     amounts = getCrossAmountsIn(amountOut, path);
+    //     require(amounts[0] <= amountInMax, 'UniswapV2Router: EXCESSIVE_INPUT_AMOUNT');
+    //     TransferHelper.safeTransferFrom(
+    //         path[0], msg.sender, _crossPairFor(path[0], path[1]), amounts[0]
+    //     );
+    //     _crossSwap(amounts, path, address(this));
+    //     IWETH(WETH).withdraw(amounts[amounts.length - 1]);
+    //     TransferHelper.safeTransferETH(to, amounts[amounts.length - 1]);
+    // }
+    // function crossSwapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+    //     external
+    //     payable
+    //     ensure(deadline)
+    //     returns (uint[] memory amounts)
+    // {
+    //     require(path[0] == WETH, 'UniswapV2Router: INVALID_PATH');
+    //     amounts = getCrossAmountsIn(amountOut, path);
+    //     require(amounts[0] <= msg.value, 'UniswapV2Router: EXCESSIVE_INPUT_AMOUNT');
+    //     IWETH(WETH).deposit{value: amounts[0]}();
+    //     assert(IWETH(WETH).transfer(_crossPairFor(path[0], path[1]), amounts[0]));
+    //     _crossSwap(amounts, path, to);
+    //     // refund dust eth, if any
+    //     if (msg.value > amounts[0]) TransferHelper.safeTransferETH(msg.sender, msg.value - amounts[0]);
+    // }
 
-    // **** LIBRARY FUNCTIONS ****
-    function quote(uint amountA, uint reserveA, uint reserveB) public pure returns (uint amountB) {
-        require(amountA > 0, 'UniswapV2Library: INSUFFICIENT_AMOUNT');
-        require(reserveA > 0 && reserveB > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
-        amountB = amountA.mul(reserveB) / reserveA;
-    }
-
-    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut)
-        public
-        view
-        returns (uint amountOut)
-    {
-        return _getAmountOut(amountIn, reserveIn, reserveOut, 4);
-    }
-
-    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut)
-        public
-        view
-        returns (uint amountIn)
-    {
-        return _getAmountIn(amountOut, reserveIn, reserveOut, 4);
-    }
-
-    function getAmountsOut(uint amountIn, address[] memory path)
-        public
-        view
-        returns (uint[] memory amounts)
-    {
-        return _getAmountsOut(amountIn, path);
-    }
-
-    function getAmountsIn(uint amountOut, address[] memory path)
-        public
-        view
-        returns (uint[] memory amounts)
-    {
-        return _getAmountsIn(amountOut, path);
-    }
-
-    function _sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
-        require(tokenA != tokenB, 'CrossRouter: IDENTICAL_ADDRESSES');
-        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'CrossRouter: ZERO_ADDRESS');
-    }
-
-    function _pairFor(address tokenA, address tokenB) internal view returns (address pair) {
+    function _crossPairFor(address tokenA, address tokenB) internal view returns (address pair) {
         address pairAddress = IUniswapV2Factory(factoryLua).getPair(tokenA, tokenB);
         if (pairAddress != address(0)) {
             return pairAddress;
@@ -190,7 +143,7 @@ contract CrossSwapRouter {
         }
     }
 
-    function _getPairInfo(address tokenA, address tokenB) internal view returns (address pair, uint swapFee, uint reserveA, uint reserveB) {
+    function _crossPairInfo(address tokenA, address tokenB) internal view returns (address pair, uint swapFee, uint reserveA, uint reserveB) {
         pair = IUniswapV2Factory(factoryLua).getPair(tokenA, tokenB);
         if (pair != address(0)) {
             swapFee = 4;
@@ -205,38 +158,14 @@ contract CrossSwapRouter {
             pair = IUniswapV2Factory(factorySushi).getPair(tokenA, tokenB);
         }
 
-        (address token0,) = _sortTokens(tokenA, tokenB);
+        (address token0,) = UniswapV2Library.sortTokens(tokenA, tokenB);
         (uint reserve0, uint reserve1,) = IUniswapV2Pair(pair).getReserves();
         (reserveA, reserveB) = tokenA == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
     }
 
-    function _getAmountOut(uint amountIn, uint reserveIn, uint reserveOut, uint fee)
-        internal
-        view
-        returns (uint amountOut)
-    {
-        require(amountIn > 0, 'UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT');
-        require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
-        uint amountInWithFee = amountIn.mul(1000 - fee);
-        uint numerator = amountInWithFee.mul(reserveOut);
-        uint denominator = reserveIn.mul(1000).add(amountInWithFee);
-        amountOut = numerator / denominator;
-    }
 
-    function _getAmountIn(uint amountOut, uint reserveIn, uint reserveOut, uint fee)
-        internal
-        view
-        returns (uint amountIn)
-    {
-        require(amountOut > 0, 'UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT');
-        require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
-        uint numerator = reserveIn.mul(amountOut).mul(1000);
-        uint denominator = reserveOut.sub(amountOut).mul(1000 - fee);
-        amountIn = (numerator / denominator).add(1);
-    }
-
-    function _getAmountsOut(uint amountIn, address[] memory path)
-        internal
+    function getCrossAmountsOut(uint amountIn, address[] memory path)
+        public
         view
         returns (uint[] memory amounts)
     {
@@ -244,13 +173,13 @@ contract CrossSwapRouter {
         amounts = new uint[](path.length);
         amounts[0] = amountIn;
         for (uint i; i < path.length - 1; i++) {
-            (, uint fee, uint reserveIn, uint reserveOut) = _getPairInfo(path[i], path[i + 1]);
-            amounts[i + 1] = _getAmountOut(amounts[i], reserveIn, reserveOut, fee);
+            (, uint fee, uint reserveIn, uint reserveOut) = _crossPairInfo(path[i], path[i + 1]);
+            amounts[i + 1] = UniswapV2Library.getAmountOut(amounts[i], reserveIn, reserveOut, fee);
         }
     }
 
-    function _getAmountsIn(uint amountOut, address[] memory path)
-        internal
+    function getCrossAmountsIn(uint amountOut, address[] memory path)
+        public
         view
         returns (uint[] memory amounts)
     {
@@ -258,8 +187,8 @@ contract CrossSwapRouter {
         amounts = new uint[](path.length);
         amounts[amounts.length - 1] = amountOut;
         for (uint i = path.length - 1; i > 0; i--) {
-            (, uint fee, uint reserveIn, uint reserveOut) = _getPairInfo(path[i - 1], path[i]);
-            amounts[i - 1] = _getAmountIn(amounts[i], reserveIn, reserveOut, fee);
+            (, uint fee, uint reserveIn, uint reserveOut) = _crossPairInfo(path[i - 1], path[i]);
+            amounts[i - 1] = UniswapV2Library.getAmountIn(amounts[i], reserveIn, reserveOut, fee);
         }
     }
 }
