f0aac732d21508a5ddc7a5eb8ce48621ed70c9df
tunght91 <hoangtung.utc@gmail.com>
2021-10-29 15:00:25 +0700

    dual farm

---
+++ contracts/dualfarm/LuaVault.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.6.12;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LuaVault is Ownable {
+    IERC20 public lua;
+    address public master;
+    
+    constructor(IERC20 _lua) public {
+        lua = _lua;
+    }
+
+    function setMaster(address _master) public onlyOwner {
+        master = _master;
+    }
+
+    function send(address _to, uint256 _amount) public {
+        require(msg.sender == owner() || msg.sender == master);
+        lua.transfer(_to, _amount);
+    }
+}
